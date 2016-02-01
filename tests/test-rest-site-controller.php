be5ea1320aa7a0eec78ea0924cc4fd31938749df
Jonathan Bardo <bardo.jonathan@gmail.com>
2016-02-01 17:17:06 -0500

    Add test_get_item_schema

--- tests/test-rest-site-controller.php
+++ tests/test-rest-site-controller.php
@@ -35,28 +35,36 @@ class WP_Test_REST_Site_Controller extends WP_Test_REST_Controller_Testcase {
 		$this->assertEquals( 200, $response->get_status() );
 	}
 
-	public function test_get_item() {
+	public function test_get_item() {}
 
-	}
+	public function test_create_item() {}
 
-	public function test_create_item() {
+	public function test_update_item() {}
 
-	}
+	public function test_delete_item() {}
 
-	public function test_update_item() {
-
-	}
-
-	public function test_delete_item() {
-
-	}
-
-	public function test_prepare_item() {
-
-	}
+	public function test_prepare_item() {}
 
 	public function test_get_item_schema() {
+		$request = new WP_REST_Request( 'OPTIONS', '/wp/v2/site' );
+		$response = $this->server->dispatch( $request );
+		$data = $response->get_data();
+		$properties = $data['schema']['properties'];
 
+		$this->assertEquals( 13, count( $properties ) );
+		$this->assertArrayHasKey( 'title', $properties );
+		$this->assertArrayHasKey( 'tagline', $properties );
+		$this->assertArrayHasKey( 'wordpress_url', $properties );
+		$this->assertArrayHasKey( 'url', $properties );
+		$this->assertArrayHasKey( 'users_can_register', $properties );
+		$this->assertArrayHasKey( 'timezone_string', $properties );
+		$this->assertArrayHasKey( 'date_format', $properties );
+		$this->assertArrayHasKey( 'time_format', $properties );
+		$this->assertArrayHasKey( 'start_of_week', $properties );
+		$this->assertArrayHasKey( 'locale', $properties );
+		$this->assertArrayHasKey( 'permalink_structure', $properties );
+		$this->assertArrayHasKey( 'permalink_category_base', $properties );
+		$this->assertArrayHasKey( 'permalink_tag_base', $properties );
 	}
 
 }
